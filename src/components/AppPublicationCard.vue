ac976e348fe14708214d9bee262393f6e0cda664
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-11-25 14:12:12 +0100

    draft of publication card

--- src/components/AppPublicationCard.vue
+++ src/components/AppPublicationCard.vue
@@ -1,21 +1,54 @@
-<script setup></script>
+<script setup>
+import { defineProps, computed, ref } from 'vue';
+const props = defineProps({
+  publication: Object,
+});
+
+const formatedDate = computed(() => {
+  const date = new Date(props.publication.user.created_at.date);
+  return date.toLocaleDateString();
+});
+
+const showFrontCamera = ref(true);
+const toggleCamera = () => {
+  showFrontCamera.value = !showFrontCamera.value;
+};
+
+const bigCamera = computed(() => {
+  return showFrontCamera.value
+    ? props.publication.frontCamera.path
+    : props.publication.backCamera.path;
+});
+
+const smallCamera = computed(() => {
+  return showFrontCamera.value
+    ? props.publication.backCamera.path
+    : props.publication.frontCamera.path;
+});
+</script>
 
 <template>
   <div class="border border-blue-500 p-1">
     <div class="flex flex-row content-center justify-between gap-2 px-1">
       <div class="aspect-square h-10 self-center rounded-full bg-white"></div>
       <div class="flex-grow">
-        <p>Stol28</p>
-        <p>Yverdon les bains</p>
+        <p>{{ publication.user.name }}</p>
+        <p>{{ formatedDate }}</p>
       </div>
       <div class="self-center">...</div>
     </div>
     <div class="relative">
-      <img
-        class="w-full object-cover"
-        src="https://picsum.photos/200/300"
-        alt="publication"
-      />
+      <div
+        class="absolute ml-4 mt-4 h-48 cursor-pointer rounded-lg bg-white shadow-lg"
+        @click="toggleCamera()"
+      >
+        <img
+          class="h-full w-full rounded-lg object-cover"
+          :src="smallCamera"
+          alt="publication"
+        />
+      </div>
+      <img class="w-full object-cover" :src="bigCamera" alt="publication" />
     </div>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,33 @@
 <script setup>
 import AppPublicationCard from '@/components/AppPublicationCard.vue';
 
-console.log('HomeView');
+const publication = {
+  frontCamera: {
+    path: 'https://picsum.photos/200/300?grayscale',
+  },
+  backCamera: {
+    path: 'https://picsum.photos/200/300',
+  },
+  user: {
+    name: 'Jérémie',
+    email: 'zurfluh.jeremie@gmail.com',
+    phone: '+41796707880',
+    profilePictureUrl: 'default.jpg',
+    created_at: {
+      date: '2024-10-02T11:44:33.450Z',
+    },
+    updated_at: {
+      date: '2024-10-02T11:44:33.450Z',
+    },
+  },
+  createdAt: new Date(),
+};
 </script>
 
 <template>
-  <main>
+  <main class="max-h-screen overflow-y-scroll">
     evneo
-    <AppPublicationCard />
+    <AppPublicationCard :publication="publication" />
   </main>
 </template>
 <style>
