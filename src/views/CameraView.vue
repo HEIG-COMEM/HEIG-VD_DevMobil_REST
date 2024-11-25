f1e0906587856111c89667d05e86f06d8607c0b9
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-11-25 16:28:22 +0100

    WIP

--- src/views/CameraView.vue
+++ src/views/CameraView.vue
@@ -1,44 +1,83 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
 import BaseCamera from '@/components/BaseCamera.vue';
 
 const camera = ref(null);
+const frontCameraBase64 = ref(null);
+const backCameraBase64 = ref(null);
+
+const cameras = ref([]);
 
 onMounted(async () => {
   const devices = await camera.value?.devices();
-  console.log(devices);
+  cameras.value = devices.filter(device => device.kind === 'videoinput');
 });
 
 const snapshot = async () => {
   const blob = await camera.value?.snapshot();
-  const url = URL.createObjectURL(blob);
-  console.log(url);
+  const base64 = await new Promise(resolve => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = () => {
+      resolve(reader.result);
+    };
+  });
+
+  if (camera.value?.facingMode === 'user') {
+    frontCameraBase64.value = base64;
+  } else {
+    backCameraBase64.value = base64;
+  }
+};
+
+const switchCamera = async deviceID => {
+  const currentDeviceID = await camera.value?.currentDeviceID();
+  console.log('Current device ID:', currentDeviceID);
+  console.log('Switching to device ID:', deviceID);
+
+  await camera.value?.changeCamera(deviceID);
 };
 </script>
 
 <template>
-  <BaseCamera
-    :resolution="{ width: 1080, height: 1920 }"
-    ref="camera"
-    autoplay
-  />
-  <button id="take-picture" @click="snapshot">📸</button>
+  <main class="max-h-screen overflow-hidden">
+    <select
+      class="select select-bordered w-full max-w-xs"
+      @change="switchCamera($event.target.value)"
+    >
+      <option
+        v-for="camera in cameras"
+        :key="camera.deviceId"
+        :value="camera.deviceId"
+      >
+        {{ camera.label }}
+      </option>
+    </select>
+    <BaseCamera
+      :resolution="{ width: 1080, height: 1920 }"
+      ref="camera"
+      autoplay
+    />
+    <button
+      id="take-picture"
+      class="btn btn-lg btn-circle btn-outline"
+      @click="snapshot()"
+    >
+      <FontAwesomeIcon :icon="faCamera" />
+    </button>
+  </main>
 </template>
 
 <style scoped>
 #take-picture {
   position: absolute;
   z-index: 99;
-  bottom: 20px;
+  bottom: 10vh;
   left: 50%;
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 50%;
-  font-size: 2rem;
-  padding: 10px;
-  height: 60px;
-  width: 60px;
 
   transform: translateX(-50%);
 }
 </style>
+<style></style>
