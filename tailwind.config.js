c251ccffb9ab178800bfaeea624b8fb1d17bda81
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-11-25 13:33:15 +0100

    Base for collaboration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#121212',
+        onSurface: '#1D1D1D',
+      }
+    },
   },
   plugins: [],
 }
