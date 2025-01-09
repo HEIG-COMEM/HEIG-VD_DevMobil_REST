4d1d9804fb634fa077b4954bc5769a28a2cac8b9
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-01-09 15:11:23 +0100

    feat(publication view): enhance publication comments and improve image handling

--- src/components/AppPublicationComments.vue
+++ src/components/AppPublicationComments.vue
@@ -1,21 +1,10 @@
 <script setup>
-import AppProfilePicture from './AppProfilePicture.vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faChevronLeft, faEllipsis } from '@fortawesome/free-solid-svg-icons';
-import { computed, ref, onMounted } from 'vue';
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
+import { computed, ref } from 'vue';
 
 const props = defineProps({
   publication: Object,
-});
-
-const formatedDate = computed(() => {
-  const date = new Date(props.publication.createdAt);
-
-  return date.toDateString() === new Date().toDateString()
-    ? `Aujourd'hui à ${date.toLocaleTimeString()}`
-    : `Il y a ${Math.floor(
-        (new Date() - date) / (1000 * 60 * 60 * 24),
-      )} jours à ${date.toLocaleTimeString()}`;
 });
 
 const locality = ref('');
@@ -30,15 +19,10 @@ const getFormatedLocation = () => {
       locality.value = data.locality;
     });
 };
-
-onMounted(() => {
-  getFormatedLocation();
-});
+getFormatedLocation();
 
 const showFrontCamera = ref(true);
-const toggleCamera = () => {
-  showFrontCamera.value = !showFrontCamera.value;
-};
+const toggleCamera = () => (showFrontCamera.value = !showFrontCamera.value);
 
 const bigCamera = computed(() => {
   return showFrontCamera.value
@@ -54,41 +38,23 @@ const smallCamera = computed(() => {
 </script>
 
 <template>
-  <div class="p-1">
-    <div class="flex flex-row items-center justify-between gap-2 p-2">
-      <button @click="$router.back()">
-        <FontAwesomeIcon class="h-6 w-6 drop-shadow-lg" :icon="faChevronLeft" />
-      </button>
-      <div class="flex flex-col items-center">
-        <div>BeReal de fhsoifhsoiefhso{{ publication.user.name }}</div>
-        <p class="text-xs text-gray-500">{{ formatedDate }}</p>
-      </div>
-      <FontAwesomeIcon class="h-6 w-6 drop-shadow-lg" :icon="faEllipsis" />
-    </div>
-    <div class="flex flex-col items-center justify-center gap-6">
-      <div>
-        <div
-          class="ml-4 mt-4 h-32 cursor-pointer rounded-lg bg-white shadow-lg"
-          @click="toggleCamera()"
-        >
-          <img
-            class="h-full w-full rounded-lg border border-black object-cover"
-            :src="smallCamera"
-            alt="publication"
-          />
-        </div>
+  <div class="flex flex-col items-center gap-4 p-1">
+    <div class="relative flex w-1/2 flex-col items-center gap-2">
+      <div
+        class="absolute left-4 top-4 h-32 cursor-pointer rounded-lg bg-white shadow-lg"
+        @click="toggleCamera()"
+      >
         <img
-          class="w-1/2 rounded-lg object-cover"
-          :src="bigCamera"
+          class="h-full w-full rounded-lg border border-black object-cover"
+          :src="smallCamera"
           alt="publication"
         />
-        <div
-          class="flex items-center justify-center gap-2 rounded-full bg-gray-800 bg-opacity-70 p-2 text-xs"
-        >
-          <FontAwesomeIcon class="h-4 w-4 drop-shadow-lg" :icon="faEllipsis" />
-          {{ locality }}
-        </div>
       </div>
+      <img class="rounded-lg object-cover" :src="bigCamera" alt="publication" />
+    </div>
+    <div class="badge badge-neutral badge-lg gap-2">
+      <FontAwesomeIcon class="h-4 w-4 drop-shadow-lg" :icon="faEllipsis" />
+      {{ locality }}
     </div>
   </div>
 </template>
