52aec018cbb2a25714590103ee45a9458c760c92
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2025-01-11 21:20:24 +0100

    feat(OneSignal): add push notification

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/styles/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
 
 import App from './App.vue';
 import router from './router';
@@ -10,5 +11,11 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(OneSignalVuePlugin, {
+  appId: import.meta.env.VITE_ONESIGNAL_APP_ID,
+  notifyButton: {
+    enable: true,
+  },
+});
 
 app.mount('#app');
